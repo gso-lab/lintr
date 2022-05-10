533f1218177dbf56a2762bac6bf7d8ebab24d159
Jonathan Kroening <jonathan.kroening@ipsos.com>
2022-05-10 10:29:22 -0700

    Different rules for spaces inside parens and brackets, based on which and based on how commas are used.

--- R/spaces_inside_linter.R
+++ R/spaces_inside_linter.R
@@ -10,21 +10,20 @@
 #' @export
 spaces_inside_linter <- function() {
   Linter(function(source_expression) {
-    tokens <- c(
+    parens_tokens <- c(
       "'('",
-      "')'",
-      "'['",
-      "']'")
+      "')'")
 
     # using a regex here as checking each token is horribly slow
     re <- rex(
-      list(one_of("[("), one_or_more(" "), none_of(end %or% "#" %or% " ")) %or%
-        list(" " %if_prev_isnt% ",", one_of("])"))
+      list("(" %if_next_is% " ", none_of(end, "#")) %or%
+        list(" " %if_prev_isnt% ",", ")")
     )
-    all_matches <- re_matches(source_expression$lines, re, global = TRUE, locations = TRUE)
+
+    all_parens_matches <- re_matches(source_expression$lines, re, global = TRUE, locations = TRUE)
     line_numbers <- as.integer(names(source_expression$lines))
 
-    Map(
+    parens_res <- Map(
       function(line_matches, line_number) {
         apply(
           line_matches,
@@ -42,18 +41,18 @@ spaces_inside_linter <- function() {
 
             if (is.na(start_of_line$start) || start_of_line$end != end) {
               pc <- source_expression$parsed_content
-              is_token <-
+              is_parens_token <-
                 any(pc[["line1"]] == line_number &
                       (pc[["col1"]] == end | pc[["col1"]] == start) &
-                      pc[["token"]] %in% tokens)
+                      pc[["token"]] %in% parens_tokens)
 
-              if (is_token) {
+              if (is_parens_token) {
                 Lint(
                   filename = source_expression$filename,
                   line_number = line_number,
                   column_number = if (substr(line, start, start) == " ") start else start + 1L,
                   type = "style",
-                  message = "Do not place spaces around code in parentheses or square brackets.",
+                  message = "Do not place spaces around code in parentheses.",
                   line = line
                 )
               }
@@ -61,8 +60,115 @@ spaces_inside_linter <- function() {
           }
         )
       },
-      all_matches,
+      all_parens_matches,
       line_numbers
     )
+
+    brackets_tokens <- c(
+      "'['",
+      "']'")
+
+    # using a regex here as checking each token is horribly slow
+    re <- rex(
+      list("[", one_or_more(" ") %if_next_isnt% ",", none_of(end %or% "#" %or% " ")) %or%
+        list(" " %if_prev_isnt% ",", "]")
+    )
+    all_brackets_matches <- re_matches(source_expression$lines, re, global = TRUE, locations = TRUE)
+    line_numbers <- as.integer(names(source_expression$lines))
+
+    brackets_res <- Map(
+      function(line_matches, line_number) {
+        apply(
+          line_matches,
+          1L,
+          function(match) {
+            start <- match[["start"]]
+            if (is.na(start)) {
+              return()
+            }
+            end <- match[["end"]]
+            line <- source_expression$lines[[as.character(line_number)]]
+
+            # make sure that the closing is not at the start of a new line (which is valid).
+            start_of_line <- re_matches(line, rex(start, spaces, one_of("])")), locations = TRUE)
+
+            if (is.na(start_of_line$start) || start_of_line$end != end) {
+              pc <- source_expression$parsed_content
+
+              is_brackets_token <-
+                any(pc[["line1"]] == line_number &
+                      (pc[["col1"]] == end | pc[["col1"]] == start) &
+                      pc[["token"]] %in% brackets_tokens)
+
+              if (is_brackets_token) {
+                Lint(
+                  filename = source_expression$filename,
+                  line_number = line_number,
+                  column_number = if (substr(line, start, start) == " ") start else start + 1L,
+                  type = "style",
+                  message = paste0("Do not place spaces around code in square brackets unless ",
+                                   "directly before or after a comma."),
+                  line = line
+                )
+              }
+            }
+          }
+        )
+      },
+      all_brackets_matches,
+      line_numbers
+    )
+
+    # using a regex here as checking each token is horribly slow
+    re <- rex(
+      list(" " %if_next_is% "[", none_of(end %or% "#" %or% " ")) %or%
+        list(" " %if_prev_isnt% ",", "]")
+    )
+    open_brackets_matches <- re_matches(source_expression$lines, re, global = TRUE, locations = TRUE)
+    line_numbers <- as.integer(names(source_expression$lines))
+
+    open_brackets_res <- Map(
+      function(line_matches, line_number) {
+        apply(
+          line_matches,
+          1L,
+          function(match) {
+            start <- match[["start"]]
+            if (is.na(start)) {
+              return()
+            }
+            end <- match[["end"]]
+            line <- source_expression$lines[[as.character(line_number)]]
+
+            # make sure that the closing is not at the start of a new line (which is valid).
+            start_of_line <- re_matches(line, rex(start, spaces, one_of("])")), locations = TRUE)
+
+            if (is.na(start_of_line$start) || start_of_line$end != end) {
+              pc <- source_expression$parsed_content
+
+              is_brackets_token <-
+                any(pc[["line1"]] == line_number &
+                      (pc[["col1"]] == end | pc[["col1"]] == start) &
+                      pc[["token"]] %in% brackets_tokens)
+
+              if (is_brackets_token) {
+                Lint(
+                  filename = source_expression$filename,
+                  line_number = line_number,
+                  column_number = if (substr(line, start, start) == " ") start else start + 1L,
+                  type = "style",
+                  message = "Do not place spaces before brackets, unless it's a closing bracket directly preceded by a comma.",
+                  line = line
+                )
+              }
+            }
+          }
+        )
+      },
+      open_brackets_matches,
+      line_numbers
+    )
+
+    return(c(parens_res, brackets_res, open_brackets_res))
   })
 }

e4e7febb1c765e2c629a2e8b7d73860416e2575f
Jonathan Kroening <jonathan.kroening@ipsos.com>
2022-05-10 11:58:30 -0700

    No trailing terminal lines, or repeated blank lines.

--- R/trailing_blank_lines_linter.R
+++ R/trailing_blank_lines_linter.R
@@ -5,13 +5,28 @@
 #' @evalRd rd_tags("trailing_blank_lines_linter")
 #' @seealso [linters] for a complete list of linters available in lintr.
 #' @export
-trailing_blank_lines_linter <- function() {
+trailing_blank_lines_linter <- function(terminal_newline = TRUE) {
   Linter(function(source_expression) {
+    # Only go over complete file
+    if (is.null(source_expression$file_lines)) return(list())
+
     blanks <- re_matches(source_expression$file_lines,
                          rex(start, any_spaces, end))
 
-    line_number <- length(source_expression$file_lines)
     lints <- list()
+
+    if (isTRUE(blanks[1])) {
+      lints[[length(lints) + 1L]] <- Lint(
+        filename = source_expression$filename,
+        line_number = 1,
+        column_number = 1,
+        type = "style",
+        message = "Blank lines at beginning of file disallowed.",
+        line = source_expression$file_lines[[1]]
+      )
+    }
+
+    line_number <- length(source_expression$file_lines)
     while (line_number > 0L && (is.na(blanks[[line_number]]) || isTRUE(blanks[[line_number]]))) {
       if (!is.na(blanks[[line_number]])) {
         lints[[length(lints) + 1L]] <- Lint(
@@ -25,18 +40,64 @@ trailing_blank_lines_linter <- function() {
       }
       line_number <- line_number - 1L
     }
-    if (identical(source_expression$terminal_newline, FALSE)) { # could use isFALSE, but needs backports
-      last_line <- tail(source_expression$file_lines, 1L)
 
-      lints[[length(lints) + 1L]] <- Lint(
-        filename = source_expression$filename,
-        line_number = length(source_expression$file_lines),
-        column_number = nchar(last_line) + 1L,
-        type = "style",
-        message = "Missing terminal newline.",
-        line = last_line
-      )
+    if (terminal_newline) {
+      if (identical(source_expression$terminal_newline, FALSE)) { # could use isFALSE, but needs backports
+        last_line <- tail(source_expression$file_lines, 1L)
+
+        lints[[length(lints) + 1L]] <- Lint(
+          filename = source_expression$filename,
+          line_number = length(source_expression$file_lines),
+          column_number = nchar(last_line) + 1L,
+          type = "style",
+          message = "Missing terminal newline.",
+          line = last_line
+        )
+      }
+    } else {
+      if (identical(source_expression$terminal_newline, TRUE)) { # could use isTRUE, but needs backports
+        last_line <- tail(source_expression$file_lines, 1L)
+
+        if (gsub(" ", "", last_line) == "") {
+          lints[[length(lints) + 1L]] <- Lint(
+            filename = source_expression$filename,
+            line_number = length(source_expression$file_lines) + 1,
+            column_number = 1,
+            type = "style",
+            message = "Terminal newline is superfluous.",
+            line = last_line
+          )
+        }
+      }
     }
+
+    runs <- rle(blanks)
+    true_runs <- runs$length[runs$values]
+
+    if (any(true_runs > 1)) { ## if there are any consecutive TRUEs (i.e. blank lines)
+      end <- cumsum(runs$lengths)
+      begin <- c(1, lag(end)[-1] + 1)
+      end_true <- end[runs$values & runs$lengths > 1]
+      begin_true <- begin[runs$values & runs$lengths > 1]
+
+      double_blanks <- unlist(lapply(
+        paste0(begin_true, ":", end_true), function(x) eval(parse(text = x))
+      ))
+
+      if (length(double_blanks) > 0) {
+        for (db in double_blanks) {
+          lints[[length(lints) + 1L]] <- Lint(
+            filename = source_expression$filename,
+            line_number = db,
+            column_number = 1,
+            type = "style",
+            message = "Consecutive blank lines are superfluous.",
+            line = source_expression$file_lines[[db]]
+          )
+        }
+      }
+    }
+
     lints
   })
 }
